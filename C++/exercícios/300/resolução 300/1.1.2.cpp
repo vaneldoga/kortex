e3bc2c871b7526477e489b2cf78f5ec73eb83144
icaro-s <santanaikaru@gmail.com>
2025-10-24 20:47:18 -0300

    Add basic input handling in 1.1.2.cpp

---
+++ C++/exercícios/300/resolução 300/1.1.2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::string nome, endereço;
+    int cep, telefone;
+    std::cin>>nome;
+    std::cin>>endereço;
+    std::cin>>cep>>telefone;     
+    return 0;
+}
